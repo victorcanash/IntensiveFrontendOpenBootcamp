b99a5dbab796402c5637b8da078afa449478dcd4
Victor Canas <victorcanas972@gmail.com>
2022-04-21 17:33:06 +0200

    Adding register and login page with its forms inside

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginForm from '../components/forms/LoginForm';
+
+
+export const LoginPage = () => {
+
+    return (
+        <div>
+            <h1>
+                Login Page
+            </h1>
+            <LoginForm />
+        </div>
+    )
+}
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegisterForm from '../components/forms/RegisterForm';
+
+
+export const RegisterPage = () => {
+
+    return (
+        <div>
+            <h1>
+                Register Page
+            </h1>
+            <RegisterForm />
+        </div>
+    )
+}
