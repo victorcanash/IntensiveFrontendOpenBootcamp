828e7931fc594d7980d5175094af61711bc1f021
Victor Canas <victorcanas972@gmail.com>
2022-04-21 17:37:26 +0200

    Adding router in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './components/forms/LoginForm';
+// React Router DOM Imports
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { KatasPage } from './pages/KatasPage';
+import { KatasDetailPage } from './pages/KatasDetailPage';
 
 
 function App() {
   return (
     <div className="App">
-        {/* Render Login Form */}
-        <LoginForm />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/register'>Register</Link>
+            </li>
+            <li>
+              <Link to='/katas'>Katas</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* TODO: Export to Routes Folder */}
+        <Routes>
+          {/* Routes definition */}
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/login' element={<LoginPage />}></Route>
+          <Route path='/register' element={<RegisterPage />}></Route>
+          <Route path='/katas' element={<KatasPage />}></Route>
+          <Route path='/katas/:id' element={<KatasDetailPage />}></Route>
+          {/* Redirecto when Page Not Found */}
+          <Route 
+            path='*' 
+            element={<Navigate to='/' replace />}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
