dfca513264404fdfabdea53e6d7db4f77d0bb1bf
Victor Canas <victorcanas972@gmail.com>
2022-05-01 11:39:06 +0200

    Adding login and register form with mui/material

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import { Dashboard } from '../components/dashboard/Dashboard';
 
 
 export const HomePage = () => {
 
     return (
-        <div>
-            <h1>
-                Home Page
-            </h1>
-        </div>
+        <Dashboard />
     )
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import LoginForm from '../components/forms/LoginForm';
+
+// import LoginForm from '../components/forms/LoginForm';
+import { LoginMaterial } from '../components/forms/LoginMaterial';
 
 
 export const LoginPage = () => {
 
     return (
         <div>
-            <h1>
-                Login Page
-            </h1>
-            <LoginForm />
+            {/* <LoginForm /> */}
+            <LoginMaterial />
         </div>
     )
 }
