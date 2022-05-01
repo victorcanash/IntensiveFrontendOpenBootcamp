2274595514697651a767ff3e0d396f8bf48d90d7
Victor Canas <victorcanas972@gmail.com>
2022-05-01 11:39:26 +0200

    Adding register material form to page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import RegisterForm from '../components/forms/RegisterForm';
+
+// import RegisterForm from '../components/forms/RegisterForm';
+import { RegisterMaterial } from '../components/forms/RegisterMaterial';
 
 
 export const RegisterPage = () => {
 
     return (
         <div>
-            <h1>
-                Register Page
-            </h1>
-            <RegisterForm />
+            {/* <RegisterForm /> */}
+            <RegisterMaterial />
         </div>
     )
 }
