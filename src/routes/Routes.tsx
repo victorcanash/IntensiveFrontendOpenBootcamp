471a23d7fb34c73666dcf7c05effb348ee422364
Victor Canas <victorcanas972@gmail.com>
2022-05-17 16:11:31 +0200

    Scroll to top on navigate

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,6 @@
-import { /* BrowserRouter as Router, */ Routes, Route, Navigate /* , Link */ } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
+import ScrollToTop from '../components/ScrollToTop';
 import { HomePage } from '../pages/HomePage';
 import { KatasDetailPage } from '../pages/KatasDetailPage';
 import { KatasPage } from '../pages/KatasPage';
@@ -9,18 +10,20 @@ import { RegisterPage } from '../pages/RegisterPage';
 
 export const AppRoutes = () => {
     return (
-        <Routes>
-            {/* Routes definition */}
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='/login' element={<LoginPage />}></Route>
-            <Route path='/register' element={<RegisterPage />}></Route>
-            <Route path='/katas' element={<KatasPage />}></Route>
-            <Route path='/katas/:id' element={<KatasDetailPage />}></Route>
-            {/* Redirecto when Page Not Found */}
-            <Route 
-                path='*' 
-                element={<Navigate to='/' replace />}>
-            </Route>
-        </Routes>
+        <ScrollToTop>
+            <Routes>
+                {/* Routes definition */}
+                <Route path='/' element={<HomePage />}></Route>
+                <Route path='/login' element={<LoginPage />}></Route>
+                <Route path='/register' element={<RegisterPage />}></Route>
+                <Route path='/katas' element={<KatasPage />}></Route>
+                <Route path='/katas/:id' element={<KatasDetailPage />}></Route>
+                {/* Redirecto when Page Not Found */}
+                <Route 
+                    path='*' 
+                    element={<Navigate to='/' replace />}>
+                </Route>
+            </Routes>
+        </ScrollToTop>
     )
 }
