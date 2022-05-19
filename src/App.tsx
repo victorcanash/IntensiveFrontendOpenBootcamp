ef7e0456371a4fc7d8b0cfed0165074cb8fb8280
Victor Canas <victorcanas972@gmail.com>
2022-05-19 17:04:07 +0200

    Init app with credentials

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,76 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
-import { AppRoutes } from './routes/Routes';
-import { StickyFooter } from './components/dashboard/StickyFooter';
-import { Loading } from './components/loading/Loading';
+
 import { ApplicationContext } from './contexts/ApplicationContext';
+import { getCredentials } from './utils/auth';
+import { IUser } from './utils/interfaces/IUser.interface';
+
+import { AppRoutes } from './routes/Routes';
+import { Loading } from './components/loading/Loading';
+import { StickyFooter } from './components/dashboard/StickyFooter';
 
 
 function App() {
 
-    const [loading, setLoadingState] = useState(true);
+    const firstRenderRef = useRef(false);
 
+    const [initialized, setInitialized] = useState(false);
+
+    const [loading, setLoadingState] = useState(true);
     const setLoading = (loading: boolean) => {
         setLoadingState(loading);
-    }
+    };
+
+    const [token, setTokenState] = useState('');
+    const setToken = (token: string) => {
+        setTokenState(token);
+    };
+
+    const [user, setUserState] = useState({} as IUser);
+    const setUser = (user: IUser) => {
+        setUserState(user);
+    };
+
+    useEffect(() => {
+        if (!firstRenderRef.current) {
+            firstRenderRef.current = true; 
+            getCredentials().then((response: {token: string, user: IUser}) => {
+                setToken(response.token);
+                setUser(response.user);
+                setInitialized(true);
+            });   
+        }    
+    }, []);
 
     return (
         <>
             {
-                loading ? 
-                    <Loading /> :
-                    null
+                loading && 
+                    <Loading />
             }
-            <div className="App">
-                <ApplicationContext.Provider
-                    value={{ loading, setLoading }}
-                >
-                    <Router>
-                        <AppRoutes />
-                    </Router>
-                    <StickyFooter />
-                </ApplicationContext.Provider>
-            </div>
+
+            {
+                initialized &&
+                    <div className="App">
+                        <ApplicationContext.Provider
+                            value={{ 
+                                loading, 
+                                token,
+                                user, 
+                                setLoading, 
+                                setToken,
+                                setUser 
+                            }}
+                        >
+                            <Router>
+                                <AppRoutes />
+                            </Router>
+                            <StickyFooter />
+                        </ApplicationContext.Provider>
+                    </div> 
+            }
         </>
     );
 }
