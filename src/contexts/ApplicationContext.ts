4149294f6341f88ec251f41791156a69926e626d
Victor Canas <victorcanas972@gmail.com>
2022-05-19 17:03:49 +0200

    Adding credentials in app context

--- src/contexts/ApplicationContext.ts
+++ src/contexts/ApplicationContext.ts
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { IUser } from '../utils/interfaces/IUser.interface';
+
 
 export const ApplicationContext = React.createContext({
     loading: true,
+    token: '',
+    user: {} as IUser,
     setLoading: (loading: boolean) => {},
+    setToken: (token: string) => {},
+    setUser: (user: IUser) => {},
 });
