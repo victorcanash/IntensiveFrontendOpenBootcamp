f9613fe43ef4ee617311519b0713f31fb22dcb21
Victor Canas <victorcanas972@gmail.com>
2022-05-19 17:03:17 +0200

    Adding storage util

---
+++ src/constants/storageKeys.ts
@@ -0,0 +1,5 @@
+enum STORAGE_KEYS {
+    JWTToken = 'sessionJWTToken'
+}
+
+export default STORAGE_KEYS;
--- src/hooks/useSessionStorage.ts
+++
@@ -1,9 +0,0 @@
-export const useSessionStorage = (key: string): any | boolean => {
-    const storedValue = sessionStorage.getItem(key);
-
-    if(!storedValue){
-        return false;
-    }else{
-        return storedValue;
-    }
-}
