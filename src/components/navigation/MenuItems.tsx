ad87c022771c7f45f9cbbb5aac8810d7d5999b90
Victor Canas <victorcanas972@gmail.com>
2022-05-25 13:10:05 +0200

    Added in menu items home and create kata nav

--- src/components/navigation/MenuItems.tsx
+++ src/components/navigation/MenuItems.tsx
@@ -4,9 +4,10 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import AddBoxSharpIcon from '@mui/icons-material/AddBoxSharp';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
-// import BarChartIcon from '@mui/icons-material/BarChart';
 
 import { ApplicationContext } from '../../contexts/ApplicationContext';
 
@@ -28,19 +29,33 @@ export const MenuItems = () => {
 
     return (
         <React.Fragment>
+            {/* Home Button */}
+            <ListItemButton onClick={() => goToPage('/')}>
+                <ListItemIcon>
+                    <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+            </ListItemButton>
+            {/* New Kata Button */}
+            <ListItemButton onClick={() => goToPage('/katas/new')}>
+                <ListItemIcon>
+                    <AddBoxSharpIcon />
+                </ListItemIcon>
+                <ListItemText primary="Create kata" />
+            </ListItemButton>
             {/* Katas Button */}
             <ListItemButton onClick={() => goToPage('/katas')}>
                 <ListItemIcon>
                     <DashboardIcon />
                 </ListItemIcon>
-                <ListItemText primary="Katas" />
+                <ListItemText primary="Find katas" />
             </ListItemButton>
             {/* Users Button */}
             <ListItemButton onClick={() => goToPage('/users')}>
                 <ListItemIcon>
                     <PeopleIcon />
                 </ListItemIcon>
-                <ListItemText primary="Users" />
+                <ListItemText primary="Find users" />
             </ListItemButton>
             {/* Ranking Button */}
             {/*<ListItemButton>
