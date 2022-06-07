683c41e1e2a7e9a152fc8bfa67625e1e9a2e87d6
Victor Canas <victorcanas972@gmail.com>
2022-06-07 13:38:53 +0200

    get user by id service

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -4,6 +4,18 @@ import axios from '../utils/config/axios.config';
 import { KataLevels } from '../utils/interfaces/IKata.interface';
 
 
+export const getUserById = (token: string, id: string) => {
+    const options: AxiosRequestConfig = {
+        headers: {
+            'x-access-token': token
+        },
+        params: {
+            id
+        }
+    }
+    return axios.get('/users', options)
+}
+
 export const getAllKatasById = (token: string, id: string, page?: number, limit?: number, level?: KataLevels, order?: {}) => {
     const options: AxiosRequestConfig = {
         headers: {
